110efa82c9afc7d5ae3562c51761fb235f3c2690
Perry <olsson.perry@gmail.com>
2021-04-22 15:07:58 -0700

    two solutions to the first challenge

---
+++ level-one.py
@@ -0,0 +1,36 @@
+testStr = "abcdabcdabcd"
+def solution(s):
+    # Your code here
+    mostSlices = 1
+    stringLength = len(s)
+    stringSlice = 1
+    while stringSlice <= stringLength / 2:
+        if stringLength % stringSlice == 0:
+            slices = stringLength / stringSlice
+            i = stringSlice
+            testSlice = s[0:stringSlice]
+            while i < stringLength:
+                if s[i:i + stringSlice] != testSlice:
+                    break
+                else:
+                    i += stringSlice
+            if i >= stringLength and slices > mostSlices:
+                mostSlices = slices
+        stringSlice += 1
+    return mostSlices
+
+print(solution(testStr))
+
+def solution2(s):
+  stringLength = len(s)
+  stringSlice = 1
+  i = stringSlice
+  while i < stringLength:
+    if stringLength % stringSlice != 0 or s[i] != s[i - stringSlice]:
+      stringSlice = i
+    i += 1
+  if stringSlice > stringLength / 2:
+    return 1
+  return stringLength / stringSlice
+
+print(solution2(testStr))
